339eb78b0cfd47dc00b15229a1d4d309b829405c
Faze250768 <mangkon.sirasit@gmail.com>
2025-06-20 03:47:31 +0000

    fff

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+
+for i in range(11):
+    result = num * i
+    print(f"{num} x {i} = {result}")
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+while True:
+    user_input = input("Say something: ")
+    
+    if user_input == "STOP":
+        break
+    
+    print(f"I got that: {user_input}")
